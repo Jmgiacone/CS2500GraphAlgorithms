fe4fd0db94f260cd20b1f4f9a65dc3a7ca6c506c
Jordan Giacone <Jmgiacone@gmail.com>
2014-12-03 19:17:21 -0600

    Testing the code of Graph

--- main.cpp
+++ main.cpp
@@ -1,35 +1,46 @@
-// Project #2
-// Jesse White, Jordan Giacone, Nick Argast
-
 #include <iostream>
-#include <random>
+#include <cstdlib>
+#include <cmath>
 #include <ctime>
+#include "Graph.h"
 #include "Node.h"
+
 using namespace std;
 
-vector<Node> testNodes;
-
-main() // purely test of node struct
+bool inRange(Node& a, Node& b, int range);
+const int AOI_LENGTH = 500, AOI_WIDTH = 500;
+int main()
 {
-    for(int i = 0; i < 10; i++)
+    srand(time(NULL));
+    int numNodes = 5, range = 250;
+    Graph g(numNodes);
+    Node* nodes = new Node[numNodes];
+
+    for(int i = 0; i < numNodes; i++)
     {
-        Node temp;
-        testNodes.push_back(temp);
-        testNodes[i].x = rand()%100;
-        testNodes[i].y = rand()%100;
-        testNodes[i].r = 50;
+        nodes[i].setId(i);
+        nodes[i].setX(rand() % AOI_WIDTH);
+        nodes[i].setY(rand() % AOI_LENGTH);
+
+        cout << nodes[i] << endl;
     }
 
-    for(int i = 0; i < 10; i++)
+    for(int i = 0; i < numNodes; i++)
     {
-        testNodes[i].Link(testNodes);
-    }
-
-    for(int i = 0; i < 10; i++)
-    {
-        for(int j = 0; j < testNodes[i].adjNodes.size(); j++)
+        for(int j = 0; j < numNodes; j++)
         {
-            std::cout << testNodes[i].x << ", " << testNodes[i].y << " is linked to " << testNodes[i].adjNodes[j].x << ", " << testNodes[i].adjNodes[j].y << ".\n";
+            if(j != i && inRange(nodes[i], nodes[j], range))
+            {
+                g.addEdge(nodes[i].id, nodes[j].id);
+            }
         }
     }
+
+    cout << g << endl;
+    return 0;
+}
+
+bool inRange(Node& a, Node& b, int range)
+{
+    return sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y)) <= range;
 }
