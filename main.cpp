7e626b5ab34a58ed599fef8deb32bc904284bf2b
Jordan Giacone <Jmgiacone@gmail.com>
2014-12-05 19:31:45 -0600

    Wrote and tested functions for number of connected components and size of largest connected component

--- main.cpp
+++ main.cpp
@@ -7,16 +7,26 @@
 
 using namespace std;
 
+void dfsVisit(Graph g, int n);
+int numConnectedComponents(Graph g);
+int numElementsInTree(Graph g, int id);
+int sizeOfLargestConnectedComponent(Graph g);
+float averageNodeDistance(Graph g);
+int diameterOfGraph(Graph g);
 bool inRange(Node& a, Node& b, int range);
 const int AOI_LENGTH = 500, AOI_WIDTH = 500;
 int main()
 {
     srand(time(NULL));
-    int numNodes = 5, range = 250;
+    int numNodes = 5, range = 100;
     Graph g(numNodes);
     Node* nodes = new Node[numNodes];
 
-    for(int i = 0; i < numNodes; i++)
+    g.addEdge(0, 1);
+    g.addEdge(1, 2);
+    g.addEdge(0, 2);
+    g.addEdge(3, 4);
+    /*for(int i = 0; i < numNodes; i++)
     {
         nodes[i].setId(i);
         nodes[i].setX(rand() % AOI_WIDTH);
@@ -34,13 +44,100 @@ int main()
                 g.addEdge(nodes[i].id, nodes[j].id);
             }
         }
-    }
+    }*/
 
     cout << g << endl;
+
+    cout << numConnectedComponents(g) << endl;
+    g.reset();
+    cout << sizeOfLargestConnectedComponent(g) << endl;
     return 0;
+}
+
+int numConnectedComponents(Graph g)
+{
+    int count = 0;
+    for(int i = 0; i < g.numVertices; i++)
+    {
+        if(!g.nodes[i]->visited)
+        {
+            dfsVisit(g, g.nodes[i]->id);
+        }
+    }
+
+    for(int i = 0; i < g.numVertices; i++)
+    {
+        if(g.nodes[i]->pi == -1)
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+void dfsVisit(Graph g, int n)
+{
+    int id;
+    g.nodes[n]->visited = true;
+    Node* tmp = g.list[n].head;
+    while(tmp != NULL)
+    {
+        id = tmp->id;
+        if(!g.nodes[id]->visited)
+        {
+            g.nodes[id]->pi = g.nodes[n]->id;
+            g.nodes[id]->visited = true;
+            dfsVisit(g, tmp->id);
+        }
+        tmp = tmp->next;
+    }
 }
 
 bool inRange(Node& a, Node& b, int range)
 {
     return sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y)) <= range;
+}
+
+int sizeOfLargestConnectedComponent(Graph g)
+{
+    int count = 1, largest = 0;
+
+    for(int i = 0; i < g.numVertices; i++)
+    {
+        if(!g.nodes[i]->visited)
+        {
+            dfsVisit(g, g.nodes[i]->id);
+        }
+    }
+
+    for(int i = 0; i < g.numVertices; i++)
+    {
+        if(g.nodes[i]->pi == -1)
+        {
+            count = numElementsInTree(g, g.nodes[i]->id);
+        }
+
+        if(count > largest)
+        {
+            largest = count;
+        }
+    }
+
+    return largest;
+}
+
+int numElementsInTree(Graph g, int id)
+{
+    int count = 0;
+
+    for(int i = 0; i < g.numVertices; i++)
+    {
+        if(g.nodes[i]->pi == id)
+        {
+            count += numElementsInTree(g, g.nodes[i]->id);
+        }
+    }
+
+    return count + 1;
 }
