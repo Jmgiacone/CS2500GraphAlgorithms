42cfbbacc96988b20f3dc823878ff68da4c34ad9
Jordan Giacone <Jmgiacone@gmail.com>
2014-12-08 00:37:12 -0600

    Removed commented-out destructor code

--- AdjacencyList.cpp
+++ AdjacencyList.cpp
@@ -8,21 +8,5 @@ AdjacencyList::AdjacencyList()
 
 AdjacencyList::~AdjacencyList()
 {
-    /*
-    AdjacencyListNode* tmp1 = head;
-    AdjacencyListNode* tmp2 = NULL;
-
-    while(tmp1 != NULL)
-    {
-        tmp2 = tmp1->next;
-
-        delete tmp1;
-
-        tmp1 = tmp2;
-    }
-
-    tmp1 = NULL;
-    tmp2 = NULL;*/
     delete head;
-
 }
