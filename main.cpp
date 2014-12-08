da7b1c0a24b5377262baa3aa1966434a56fe087f
Jordan Giacone <Jmgiacone@gmail.com>
2014-12-07 20:00:35 -0600

    Added looping code. There is a problem where deleting a graph pointer will cause a segfault

--- main.cpp
+++ main.cpp
@@ -2,84 +2,105 @@
 #include <cstdlib>
 #include <cmath>
 #include <ctime>
+#include <fstream>
 #include "Graph.h"
 #include "Node.h"
 
 using namespace std;
 
-void dfsVisit(Graph g, int n);
+void dfsVisit(Graph* g, int n);
 void parray (int **a, int b);
-int numConnectedComponents(Graph g);
-float aosp(Graph g);
-int numElementsInTree(Graph g, int id);
-int sizeOfLargestConnectedComponent(Graph g);
-float averageNodeDistance(Graph g);
-int diameterOfGraph(Graph g);
-bool inRange(Node& a, Node& b, int range);
-const int AOI_LENGTH = 500, AOI_WIDTH = 500;
+int numConnectedComponents(Graph* g);
+float aosp(Graph* g, int& totalDiameter);
+int numElementsInTree(Graph* g, int id);
+int sizeOfLargestConnectedComponent(Graph* g);
+float averageNodeDistance(Graph* g);
+int diameterOfGraph(Graph* g);
+bool inRange(Node* a, Node* b, int range);
+const int AOI_LENGTH = 500, AOI_WIDTH = 500, MIN_NODES = 50, MAX_NODES = 1000, NUM_REPETITIONS = 15, ARBITRARY_RANGE = 20;
+const string FILENAME = "GraphTestingSuite.txt";//"H:\\Users\\Jordan\\Documents\\Github Repos\\CS2500GraphAlgorithms\\GraphTestingSuite.txt";
 
 int main()
 {
     srand(time(NULL));
-    int numNodes = 10, range = 100;
+    int numNodes = 0, range = ARBITRARY_RANGE, totalConnectedComponents = 0,
+            totalLargestConnectedComponent = 0, totalAverageDistance = 0, totalDiameter = 0;
+    ofstream outputFile;
+    outputFile.open(FILENAME.c_str());
+    Graph* g;
+    Node** nodes;
+    outputFile << "VARYING NUMBER OF NODES WITH TRANSMISSION RANGE OF " << ARBITRARY_RANGE << endl;
 
-    Graph g(numNodes);
-    Node* nodes = new Node[numNodes];
+    g = new Graph(5);
+    g->addEdge(0, 1);
+    g->addEdge(1, 2);
+    g->addEdge(2, 3);
+    g->addEdge(3, 4);
+    g->addEdge(4, 0);
 
-    g.addEdge(0, 4);
-    g.addEdge(2, 4);
-    g.addEdge(4, 5);
-    g.addEdge(5, 8);
-    g.addEdge(3, 8);
-    g.addEdge(6, 7);
-    g.addEdge(7, 9);
-    g.addEdge(1, 0);
-
-    /*for(int i = 0; i < numNodes; i++)
+    cout << *g << endl;
+    delete g;
+    while(numNodes != MAX_NODES)
     {
-        nodes[i].setId(i);
-        nodes[i].setX(rand() % AOI_WIDTH);
-        nodes[i].setY(rand() % AOI_LENGTH);
-
-        cout << nodes[i] << endl;
-    }
-
-    for(int i = 0; i < numNodes; i++)
-    {
-        for(int j = 0; j < numNodes; j++)
+        numNodes += MIN_NODES;
+        outputFile << "Number of Nodes: " << numNodes << endl;
+        for(int i = 0; i < NUM_REPETITIONS; i++)
         {
-            if(j != i && inRange(nodes[i], nodes[j], range))
+            g = new Graph(numNodes);
+            nodes = new Node*[numNodes];
+
+            for (int i = 0; i < numNodes; i++)
             {
-                g.addEdge(nodes[i].id, nodes[j].id);
+                nodes[i] = new Node(i,rand() % AOI_WIDTH,rand() % AOI_LENGTH);
             }
+
+            for (int i = 0; i < numNodes; i++) {
+                for (int j = 0; j < numNodes; j++) {
+                    if (j != i && inRange(nodes[i], nodes[j], range)) {
+                        g->addEdge(nodes[i]->id, nodes[j]->id);
+                    }
+                }
+            }
+
+            for(int i = 0; i < numNodes; i++)
+            {
+                delete nodes[i];
+            }
+            delete [] nodes;
+            nodes = NULL;
+            totalConnectedComponents += numConnectedComponents(g);
+            g->reset();
+            totalLargestConnectedComponent += sizeOfLargestConnectedComponent(g);
+            g->reset();
+            totalAverageDistance += aosp(g, totalDiameter);
+
+            delete g;
+            g = NULL;
         }
-    }*/
 
-    cout << g << endl;
-
-    cout << numConnectedComponents(g) << endl;
-    g.reset();
-    cout << sizeOfLargestConnectedComponent(g) << endl;
-
-    cout << "\nAoSP result: " << aosp(g) << "\n";
+        outputFile << "Average number of Connected Components: " << totalConnectedComponents / static_cast<float>(NUM_REPETITIONS) << endl;
+        outputFile << "Average size of the largest connected Component: " << totalLargestConnectedComponent / static_cast<float>(NUM_REPETITIONS) << endl;
+        outputFile << "Average of the average node distance: " << totalAverageDistance / static_cast<float>(NUM_REPETITIONS) << endl;
+        outputFile << "Average Diameter: " << totalDiameter / static_cast<float>(NUM_REPETITIONS) << endl;
+    }
 
     return 0;
 }
 
-int numConnectedComponents(Graph g)
+int numConnectedComponents(Graph* g)
 {
     int count = 0;
-    for(int i = 0; i < g.numVertices; i++)
+    for(int i = 0; i < g->numVertices; i++)
     {
-        if(!g.nodes[i]->visited)
+        if(!g->nodes[i]->visited)
         {
-            dfsVisit(g, g.nodes[i]->id);
+            dfsVisit(g, g->nodes[i]->id);
         }
     }
 
-    for(int i = 0; i < g.numVertices; i++)
+    for(int i = 0; i < g->numVertices; i++)
     {
-        if(g.nodes[i]->pi == -1)
+        if(g->nodes[i]->pi == -1)
         {
             count++;
         }
@@ -88,46 +109,46 @@ int numConnectedComponents(Graph g)
     return count;
 }
 
-void dfsVisit(Graph g, int n)
+void dfsVisit(Graph* g, int n)
 {
     int id;
-    g.nodes[n]->visited = true;
-    Node* tmp = g.list[n].head;
+    g->nodes[n]->visited = true;
+    AdjacencyListNode* tmp = g->list[n]->head;
     while(tmp != NULL)
     {
         id = tmp->id;
-        if(!g.nodes[id]->visited)
+        if(!g->nodes[id]->visited)
         {
-            g.nodes[id]->pi = g.nodes[n]->id;
-            g.nodes[id]->visited = true;
+            g->nodes[id]->pi = g->nodes[n]->id;
+            g->nodes[id]->visited = true;
             dfsVisit(g, tmp->id);
         }
         tmp = tmp->next;
     }
 }
 
-bool inRange(Node& a, Node& b, int range)
+bool inRange(Node* a, Node* b, int range)
 {
-    return pow((a.x - b.x),2) + pow((a.y - b.y) , 2) <= pow(range,2);
+    return pow((a->x - b->x),2) + pow((a->y - b->y) , 2) <= pow(range,2);
 }
 
-int sizeOfLargestConnectedComponent(Graph g)
+int sizeOfLargestConnectedComponent(Graph* g)
 {
     int count = 1, largest = 0;
 
-    for(int i = 0; i < g.numVertices; i++)
+    for(int i = 0; i < g->numVertices; i++)
     {
-        if(!g.nodes[i]->visited)
+        if(!g->nodes[i]->visited)
         {
-            dfsVisit(g, g.nodes[i]->id);
+            dfsVisit(g, g->nodes[i]->id);
         }
     }
 
-    for(int i = 0; i < g.numVertices; i++)
+    for(int i = 0; i < g->numVertices; i++)
     {
-        if(g.nodes[i]->pi == -1)
+        if(g->nodes[i]->pi == -1)
         {
-            count = numElementsInTree(g, g.nodes[i]->id);
+            count = numElementsInTree(g, g->nodes[i]->id);
         }
 
         if(count > largest)
@@ -139,25 +160,25 @@ int sizeOfLargestConnectedComponent(Graph g)
     return largest;
 }
 
-int numElementsInTree(Graph g, int id)
+int numElementsInTree(Graph* g, int id)
 {
     int count = 0;
 
-    for(int i = 0; i < g.numVertices; i++)
+    for(int i = 0; i < g->numVertices; i++)
     {
-        if(g.nodes[i]->pi == id)
+        if(g->nodes[i]->pi == id)
         {
-            count += numElementsInTree(g, g.nodes[i]->id);
+            count += numElementsInTree(g, g->nodes[i]->id);
         }
     }
 
     return count + 1;
 }
 
-float aosp(Graph g)//average of shortest paths
+float aosp(Graph* g, int& totalDiameter)//average of shortest paths
 {
-    cout << "break";
-    int numVerts = g.numVertices;
+    //cout << "break";
+    int numVerts = g->numVertices;
 
     int **aosparray;
 
@@ -181,7 +202,7 @@ float aosp(Graph g)//average of shortest paths
             }
         }
 
-        Node* temp = g.list[i].head;
+        AdjacencyListNode* temp = g->list[i]->head;
 
         while(temp != NULL)
         {
@@ -204,7 +225,7 @@ float aosp(Graph g)//average of shortest paths
         }
 
     }
-    parray(aosparray, numVerts);
+    //parray(aosparray, numVerts);
 
     float sum = 0;
     float div = 0;
@@ -221,15 +242,13 @@ float aosp(Graph g)//average of shortest paths
 
                 if(aosparray[i][j] > d)
                 {
-                    d = aosparray[i][j];
+                    totalDiameter += aosparray[i][j];
                 }
             }
         }
     }
 
-    cout << "\nDiameter = " << d << "\n\n";
-
-    return (sum/div);
+    return (div == 0 ? 0 : sum/div);
 
 
 }
